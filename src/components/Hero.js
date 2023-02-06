2f13192ef0411b382c5602260b9c73a25cfb6ef6
Emre Karayaman <karayaman.emre@outlook.com>
2023-02-06 13:09:29 +0300

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className={styles.hero_banner}>
         <h1>I'm Emre<br></br>Glad to see you!</h1>
         <p>
-          I work on developing software! I can help you make a product, a website, or a feature for it. Check out some of my work and what I've done! If you like what you see and need coding for a project, don't be afraid to get in touch.
+          I design and develop software! I can help you with developing a product, website, or feature for it. Examine some of my work and what I've done! If you like what you see and require coding for a project, feel free to contact me.
         </p>
         <a href='#portfolio' className={[styles.hero_btn, 'btn'].join(' ')}>
           portfolio
