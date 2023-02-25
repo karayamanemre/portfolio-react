b477833ab7edb32e66719c304f9992e5630109ea
Emre Karayaman <107217989+karayamanemre@users.noreply.github.com>
2023-02-25 19:57:36 +0300

    mobile social links added to toggle menu

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,40 @@
-import React from 'react'
-import styles from './styles/About.module.css'
-import { techStack } from '../data'
-import Title from './Title'
+import React from 'react';
+import styles from './styles/About.module.css';
+import { techStack } from '../data';
+import Title from './Title';
 
 const About = () => {
   return (
-    <section className='section' id='about'>
-      <Title title='About' subTitle='Me'/>
-      <div className={[styles.about_center, "section-center"].join(' ')}>
+    <section className="section" id="about">
+      <Title title="About" subTitle="Me" />
+      <div className={[styles.about_center, 'section-center'].join(' ')}>
         <article className={styles.about_info}>
           <p>
-            I attended Microverse Coding School and participated in daily remote team programming sessions. 
-          </p>
-          <p>  
-            Before software development, I managed pubs and restaurants. Although not development-related, I learned problem-solving, effective communication, multitasking, and more. I've been rewarded for my quality and customer service, and I bring the same passion to my software engineering. I'm also a musician and composer. I produce my own music from scratch. I want to use my creativity to design software.
+            I attended Microverse Coding School and participated in daily remote
+            team programming sessions.
           </p>
           <p>
-            I've done a lot of remote work with coworkers in other time zones and cultures.
-            Feel free to get in touch if you're interested and could use some help with a coding project.
+            Before software development, I managed pubs and restaurants.
+            Although not development-related, I learned problem-solving,
+            effective communication, multitasking, and more. I've been rewarded
+            for my quality and customer service, and I bring the same passion to
+            my software engineering. I'm also a musician and composer. I produce
+            my own music from scratch. I want to use my creativity to design
+            software.
           </p>
-          <form method="get" action="https://karayamanemre.github.io/emre-portfolio/assets/EmreKarayamanResume.pdf" target='_blank'>
-            <button type="submit" className='btn' target='_blank'>Get my resume</button>
+          <p>
+            I've done a lot of remote work with coworkers in other time zones
+            and cultures. Feel free to get in touch if you're interested and
+            could use some help with a coding project.
+          </p>
+          <form
+            method="get"
+            action="https://karayamanemre.github.io/emre-portfolio/assets/EmreKarayamanResume.pdf"
+            target="_blank"
+          >
+            <button type="submit" className="btn" target="_blank">
+              Get my resume
+            </button>
           </form>
         </article>
         <div className={styles.about_skills}>
@@ -28,19 +42,19 @@ const About = () => {
             {techStack.map((tech) => {
               return (
                 <li className={styles.skill_item} key={tech.id}>
-                  <img src={tech.img} alt='logo' />
+                  <img src={tech.img} alt="logo" />
                   <p>{tech.text}</p>
                 </li>
-              )
+              );
             })}
           </ul>
         </div>
       </div>
-      <a href="https://github.com/karayamanemre" target='_blank' rel="noopener noreferrer">
+      {/* <a href="https://github.com/karayamanemre" target='_blank' rel="noopener noreferrer">
         <img className={styles.gh_chart} src="https://ghchart.rshah.org/288AAD/karayamanemre" alt="EmreKarayaman's Github chart" />
-      </a>
+      </a> */}
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import styles from './styles/Navbar.module.css'
-import PageLinks from './PageLinks'
-import SocialLinks from './SocialLinks'
+import React, { useState, useEffect } from 'react';
+import styles from './styles/Navbar.module.css';
+import PageLinks from './PageLinks';
+import SocialLinks from './SocialLinks';
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
@@ -10,13 +10,13 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-  const handleClick = (event) => {
-    if (openMenu && !event.target.closest(`.${styles.nav_header}`)) {
-      toggleMenu();
-    }
-  };
-  document.addEventListener('click', handleClick);
-  return () => document.removeEventListener('click', handleClick);
+    const handleClick = (event) => {
+      if (openMenu && !event.target.closest(`.${styles.nav_header}`)) {
+        toggleMenu();
+      }
+    };
+    document.addEventListener('click', handleClick);
+    return () => document.removeEventListener('click', handleClick);
   }, [openMenu]);
 
   return (
@@ -24,20 +24,38 @@ const Navbar = () => {
       <div className={styles.nav_center}>
         <div className={styles.nav_header}>
           <span>Emre Karayaman</span>
-          <button type='button' className={styles.nav_toggle} onClick={toggleMenu}>
-            <i className={openMenu ? 'fas fa-xmark': 'fas fa-bars'} onClick={toggleMenu}></i>
-          </button>  
+          <button
+            type="button"
+            className={styles.nav_toggle}
+            onClick={toggleMenu}
+          >
+            <i
+              className={openMenu ? 'fas fa-xmark' : 'fas fa-bars'}
+              onClick={toggleMenu}
+            ></i>
+          </button>
           {openMenu ? (
             <div className={styles.toggle_menu}>
-              <PageLinks parentClass={''} itemClass={styles.nav_link} onClick={toggleMenu}/>
+              <PageLinks
+                parentClass={styles.links_mobile}
+                itemClass={styles.nav_link}
+                onClick={toggleMenu}
+              />
+              <SocialLinks
+                parentClass={styles.social_mobile}
+                itemClass={styles.nav_icon}
+              />
             </div>
           ) : null}
         </div>
-        <PageLinks parentClass={styles.nav_links} itemClass={styles.nav_link}/>
-        <SocialLinks parentClass={styles.nav_icons} itemClass={styles.nav_icon}/>
+        <PageLinks parentClass={styles.nav_links} itemClass={styles.nav_link} />
+        <SocialLinks
+          parentClass={styles.nav_icons}
+          itemClass={styles.nav_icon}
+        />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
