d4e3a7b38c8eef34bcb624379aedce4a132d9e1e
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-06-12 15:22:37 +0300

    social links added to contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Title from './Title';
+import SocialLinks from './SocialLinks';
 import styles from './styles/Contact.module.css';
 
 const Contact = () => {
   return (
-    <section className="section" id="contact">
-      <Title title="Contact" subTitle="Me" />
-      <div className="section-center">
+    <section className='section' id='contact'>
+      <Title title='Contact' subTitle='Me' />
+      <div className='section-center'>
         <p className={styles.contact_text}>
           If you have an application you are interested in developing, a feature
           that you need built or a project that needs coding.<br></br>I’d love
@@ -14,49 +15,53 @@ const Contact = () => {
         </p>
         <form
           className={styles.contact_form}
-          action="https://formspree.io/f/xzbwzdvd"
-          method="post"
-          id="form"
-          target="_blank"
+          action='https://formspree.io/f/xzbwzdvd'
+          method='post'
+          id='form'
+          target='_blank'
         >
-          <label htmlFor="contact-name" hidden>
+          <label htmlFor='contact-name' hidden>
             Name
           </label>
           <input
-            type="text"
-            name="contact-name"
-            placeholder="Your Name"
+            type='text'
+            name='contact-name'
+            placeholder='Your Name'
             className={styles.contact_element}
-            id="contact-name"
-            maxLength="30"
+            id='contact-name'
+            maxLength='30'
             required
           />
-          <label htmlFor="contact-email" hidden>
+          <label htmlFor='contact-email' hidden>
             Email
           </label>
           <input
-            type="email"
-            name="contact-mail"
-            placeholder="Your Email"
+            type='email'
+            name='contact-mail'
+            placeholder='Your Email'
             className={styles.contact_element}
-            id="contact-email"
+            id='contact-email'
             required
           />
-          <label htmlFor="contact-message" hidden>
+          <label htmlFor='contact-message' hidden>
             Message
           </label>
           <textarea
-            name="contact-message"
-            id="contact-message"
+            name='contact-message'
+            id='contact-message'
             className={styles.contact_element}
-            maxLength="500"
-            placeholder="Your Message"
+            maxLength='500'
+            placeholder='Your Message'
             required
           ></textarea>
-          <button type="submit" className="btn">
+          <button type='submit' className='btn'>
             Get In Touch
           </button>
         </form>
+        <SocialLinks
+          parentClass={styles.contact_social_links}
+          itemClass={styles.contact_social_link}
+        />
       </div>
     </section>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import PageLinks from './PageLinks'
-import SocialLinks from './SocialLinks'
-import styles from './styles/Footer.module.css'
+import React from 'react';
+import PageLinks from './PageLinks';
+import SocialLinks from './SocialLinks';
+import styles from './styles/Footer.module.css';
 
 const Footer = () => {
   return (
     <footer className={[styles.footer, 'section'].join(' ')}>
-      <PageLinks parentClass={styles.footer_links} itemClass={styles.footer_link}/>
-      <SocialLinks parentClass={styles.footer_icons} itemClass={styles.footer_icon}/>
+      <PageLinks
+        parentClass={styles.footer_links}
+        itemClass={styles.footer_link}
+      />
+
       <p className={styles.copyright}>
         Emre Karayaman
-        <span id='date'>
-          {new Date().getFullYear()}
-        </span>
+        <span id='date'>{new Date().getFullYear()}</span>
       </p>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
