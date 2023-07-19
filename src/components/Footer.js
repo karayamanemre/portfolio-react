5661202410b421e58843a5d4f02b71cc22cd0c91
Emre Karayaman <karayaman.emre@outlook.com>
2023-07-19 14:19:43 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import PageLinks from './PageLinks';
-import SocialLinks from './SocialLinks';
-import styles from './styles/Footer.module.css';
+import React from "react";
+import PageLinks from "./PageLinks";
+import styles from "./styles/Footer.module.css";
 
 const Footer = () => {
-  return (
-    <footer className={[styles.footer, 'section'].join(' ')}>
-      <PageLinks
-        parentClass={styles.footer_links}
-        itemClass={styles.footer_link}
-      />
+	return (
+		<footer className={[styles.footer, "section"].join(" ")}>
+			<PageLinks
+				parentClass={styles.footer_links}
+				itemClass={styles.footer_link}
+			/>
 
-      <p className={styles.copyright}>
-        Emre Karayaman
-        <span id='date'>{new Date().getFullYear()}</span>
-      </p>
-    </footer>
-  );
+			<p className={styles.copyright}>
+				Emre Karayaman
+				<span id='date'>{new Date().getFullYear()}</span>
+			</p>
+		</footer>
+	);
 };
 
 export default Footer;
